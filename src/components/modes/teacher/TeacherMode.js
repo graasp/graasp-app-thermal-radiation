39884e3a00461fc5ca78869b4a0cf499597b50ea
pyphilia <pyphilia@gmail.com>
2022-10-31 15:51:54 +0100

    refactor: remove loaders in screens

--- src/components/modes/teacher/TeacherMode.js
+++ src/components/modes/teacher/TeacherMode.js
@@ -3,25 +3,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import TeacherView from './TeacherView';
 import { DEFAULT_VIEW, DASHBOARD_VIEW } from '../../../config/views';
-import Loader from '../../common/Loader';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TeacherMode extends Component {
   static propTypes = {
     view: PropTypes.string,
-    activity: PropTypes.bool,
   };
 
   static defaultProps = {
     view: DEFAULT_VIEW,
-    activity: false,
   };
 
   render() {
-    const { view, activity } = this.props;
-    if (activity) {
-      return <Loader />;
-    }
+    const { view } = this.props;
     switch (view) {
       case DASHBOARD_VIEW:
       case DEFAULT_VIEW:
